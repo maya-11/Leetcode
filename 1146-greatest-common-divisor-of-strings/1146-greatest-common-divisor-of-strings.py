49f70b864f2a3a1cf5b418a21efcbebdddb17d40
maya-11 <141321191+maya-11@users.noreply.github.com>
2025-02-11 00:38:24 +0200

    Time: 0 ms (100%), Space: 12.6 MB (19.78%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        def gcd(a, b):
+            if b == 0:
+                return a
+            return gcd(b, a % b)
+        
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        gcd_length = gcd(len(str1), len(str2))
+        return str1[:gcd_length]
