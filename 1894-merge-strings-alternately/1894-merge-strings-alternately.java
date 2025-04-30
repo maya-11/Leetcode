488803113256d6e4d12d87c65cee1776d780e32a
maya-11 <141321191+maya-11@users.noreply.github.com>
2025-04-30 19:54:51 +0200

    Time: 1 ms (82.13%), Space: 41.3 MB (99.42%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -2,22 +2,22 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         StringBuilder merged = new StringBuilder();
         int i = 0, j = 0;
-        
-        // Merge letters alternately
-        while (i < word1.length() && j < word2.length()) {
+        int len1 = word1.length(), len2 = word2.length();
+
+        while (i < len1 && j < len2) {
             merged.append(word1.charAt(i++));
             merged.append(word2.charAt(j++));
         }
-        
-        // Append the rest of the longer string
-        while (i < word1.length()) {
+
+        // Append the rest of the characters if any
+        while (i < len1) {
             merged.append(word1.charAt(i++));
         }
-        
-        while (j < word2.length()) {
+
+        while (j < len2) {
             merged.append(word2.charAt(j++));
         }
-        
+
         return merged.toString();
     }
 }
