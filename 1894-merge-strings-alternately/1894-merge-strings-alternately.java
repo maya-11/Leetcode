28193e2d0a0c8273912c0053e9acc33e43067540
maya-11 <141321191+maya-11@users.noreply.github.com>
2025-08-10 23:48:53 +0200

    Time: 1 ms (82.44%), Space: 41.6 MB (93.84%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -4,16 +4,18 @@ class Solution {
         int i = 0, j = 0;
         int len1 = word1.length(), len2 = word2.length();
 
+        // Alternate characters from both strings
         while (i < len1 && j < len2) {
             merged.append(word1.charAt(i++));
             merged.append(word2.charAt(j++));
         }
 
-        // Append the rest of the characters if any
+        // Append leftovers from word1
         while (i < len1) {
             merged.append(word1.charAt(i++));
         }
 
+        // Append leftovers from word2
         while (j < len2) {
             merged.append(word2.charAt(j++));
         }
